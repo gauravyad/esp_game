d02208355f7c05fe92829dfe7fe2cdd824c49a9f
gauravyad <g236yadav@gmail.com>
2017-09-02 22:57:24 +0530

    Add files via upload

--- static/game.js
+++ static/game.js
@@ -1,12 +1,20 @@
 var socket = io();
 var score=0;
-socket.on('startMatch', function() {
+var i=0;
+var arr={};
+socket.on('startMatch', function(data) {
   console.log("Match Started");
+    arr=data;
+    var cur=data[i];
+    var image=document.getElementById('primary');
+    image.src='static/images/'+cur.toString()+'.jpeg';
+    var secon_1=document.getElementById('sec_1');
+    var secon_2=document.getElementById('sec_2');
+    secon_1.src='static/images/'+cur.toString()+'_1.jpeg';
+    secon_2.src='static/images/'+cur.toString()+'_2.jpeg';
 });
-socket.on('ansMatch', function() {
-  console.log("Ans Matched");
-  score++;
-  document.getElementById('score').value=score;
+socket.on('score', function(data) {
+  document.getElementById('score').value=data;
 });
 var answers = {
   one: '-',
@@ -37,13 +45,25 @@ function option_b_selected(){
   answers[cur]='B';
   console.log(answers[cur])
   alert('B selected')
-  socket.emit('answers',answers,cur)
   
 }
 function option_a_selected(){
   answers[cur]='A';
   console.log(answers[cur])
   alert('A selected')
-  socket.emit('answers',answers,cur)
-  
+}
+function on_submit(){
+  i++;
+  if(i<5){
+    var cur=arr[i];
+    var image=document.getElementById('primary');
+    image.src='static/images/'+cur.toString()+'.jpeg';
+    var secon_1=document.getElementById('sec_1');
+    var secon_2=document.getElementById('sec_2');
+    secon_1.src='static/images/'+cur.toString()+'_1.jpeg';
+    secon_2.src='static/images/'+cur.toString()+'_2.jpeg';
+  }
+  else{
+    socket.emit('answers',answers,arr)
+  }
 }
